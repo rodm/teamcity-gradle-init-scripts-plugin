e3d69a26f3c8b0dde60bf46d3678d617efdc6dd8
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2025-07-17 09:16:41 +0100

    Update test environment to use latest TeamCity version

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -11,8 +11,8 @@ dependencies {
 
 teamcity {
     environments {
-        register("teamcity2024.12") {
-            version = "2024.12"
+        register("teamcity") {
+            version = "2025.03.3"
             plugins = configurations["plugins"]
             serverOptions ("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005")
             agentOptions ("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006")
