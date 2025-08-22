a6216cce0c53586e8eddedad99a201eaaa7b9a66
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2025-08-22 11:22:07 +0100

    Re-organise tests and improve coverage

--- server/src/test/groovy/com/github/rodm/teamcity/gradle/scripts/server/DeleteScriptActionTest.groovy
+++ server/src/test/groovy/com/github/rodm/teamcity/gradle/scripts/server/DeleteScriptActionTest.groovy
@@ -16,8 +16,10 @@
 
 package com.github.rodm.teamcity.gradle.scripts.server
 
+import jetbrains.buildServer.controllers.ActionMessages
 import jetbrains.buildServer.serverSide.ProjectManager
 import jetbrains.buildServer.serverSide.SProject
+import jetbrains.buildServer.serverSide.auth.AccessDeniedException
 import jetbrains.buildServer.serverSide.auth.AuthorityHolder
 import jetbrains.buildServer.serverSide.auth.Permission
 import jetbrains.buildServer.serverSide.auth.SecurityContext
@@ -30,8 +32,9 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import javax.servlet.http.HttpSession
 
-import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.CoreMatchers.equalTo
 import static org.hamcrest.CoreMatchers.is
+import static org.hamcrest.MatcherAssert.assertThat
 import static org.mockito.ArgumentMatchers.any
 import static org.mockito.ArgumentMatchers.anyString
 import static org.mockito.ArgumentMatchers.eq
@@ -44,21 +47,29 @@ import static org.mockito.Mockito.when
 class DeleteScriptActionTest {
 
     private InitScriptsActionsController actionsController
-    private ProjectManager projectManager
-    private GradleScriptsManager scriptsManager
-    private SecurityContext securityContext
+    private ProjectManager projectManager = mock(ProjectManager)
+    private GradleScriptsManager scriptsManager = mock(GradleScriptsManager)
+    private SecurityContext securityContext = mock(SecurityContext)
+    private AuthorityHolder authorityHolder = mock(AuthorityHolder)
+
+    private HttpServletRequest request = mock(HttpServletRequest)
+    private HttpServletResponse response = mock(HttpServletResponse)
+    private HttpSession session = mock(HttpSession)
+
     private Element ajaxResponse = new Element("response")
+    private ActionMessages messages = new ActionMessages()
 
     @BeforeEach
     void setup() {
         WebControllerManager controllerManager = mock(WebControllerManager)
         actionsController = new InitScriptsActionsController(controllerManager)
-        projectManager = mock(ProjectManager)
-        scriptsManager = mock(GradleScriptsManager)
-        securityContext = mock(SecurityContext)
-        AuthorityHolder authorityHolder = mock(AuthorityHolder)
         when(authorityHolder.isPermissionGrantedForProject(anyString(), eq(Permission.EDIT_PROJECT))).thenReturn(true)
         when(securityContext.getAuthorityHolder()).thenReturn(authorityHolder)
+
+        when(request.getSession()).thenReturn(session)
+        when(request.getParameter(eq('name'))).thenReturn('init.gradle')
+        when(request.getParameter(eq('projectId'))).thenReturn('projectId')
+        when(request.getAttribute('actionMessages')).thenReturn(messages)
     }
 
     private DeleteScriptAction createDeleteAction() {
@@ -76,8 +87,6 @@ class DeleteScriptActionTest {
     @Test
     void 'can handle deleteScript action'() {
         DeleteScriptAction action = createDeleteAction()
-
-        HttpServletRequest request = mock(HttpServletRequest)
         when(request.getParameter(eq('action'))).thenReturn('deleteScript')
 
         assertThat(action.canProcess(request), is(true))
@@ -86,16 +95,8 @@ class DeleteScriptActionTest {
     @Test
     void 'deletes a script from a project'() {
         DeleteScriptAction action = createDeleteAction()
-
-        HttpServletRequest request = mock(HttpServletRequest)
-        HttpServletResponse response = mock(HttpServletResponse)
-        HttpSession session = mock(HttpSession)
-
-        when(request.getSession()).thenReturn(session)
-        when(request.getParameter(eq('name'))).thenReturn('init.gradle')
-        when(request.getParameter(eq('projectId'))).thenReturn('project1')
         SProject project = mock(SProject)
-        when(projectManager.findProjectById('project1')).thenReturn(project)
+        when(projectManager.findProjectById('projectId')).thenReturn(project)
 
         action.process(request, response, ajaxResponse)
 
@@ -103,18 +104,72 @@ class DeleteScriptActionTest {
     }
 
     @Test
-    void 'does not delete a script for an invalid project id'() {
+    void 'delete action message reports file deletion is scheduled'() {
         DeleteScriptAction action = createDeleteAction()
+        when(projectManager.findProjectById('projectId')).thenReturn(mock(SProject))
 
-        HttpServletRequest request = mock(HttpServletRequest)
-        HttpServletResponse response = mock(HttpServletResponse)
+        action.process(request, response, ajaxResponse)
 
-        when(request.getParameter(eq('name'))).thenReturn('init.gradle')
-        when(request.getParameter(eq('projectId'))).thenReturn('project1')
-        when(projectManager.findProjectById('project1')).thenReturn(null)
+        def messages = ActionMessages.getMessages(request)
+        def expectedMessage = 'Gradle init script init.gradle has been scheduled for deletion'
+        assertThat(messages.getMessage('initScriptsMessage'), equalTo(expectedMessage))
+    }
+
+    @Test
+    void 'delete action message reports file deletion is completed'() {
+        DeleteScriptAction action = createDeleteAction()
+        when(projectManager.findProjectById('projectId')).thenReturn(mock(SProject))
+        when(scriptsManager.deleteScript(any(), eq('init.gradle'))).thenReturn(true)
+
+        action.process(request, response, ajaxResponse)
+
+        def messages = ActionMessages.getMessages(request)
+        def expectedMessage = 'Gradle init script init.gradle was deleted'
+        assertThat(messages.getMessage('initScriptsMessage'), equalTo(expectedMessage))
+    }
+
+    @Test
+    void 'invalid project id does not delete script'() {
+        DeleteScriptAction action = createDeleteAction()
+        when(projectManager.findProjectById('projectId')).thenReturn(null)
 
         action.process(request, response, ajaxResponse)
 
         verify(scriptsManager, never()).deleteScript(any(SProject), anyString())
+    }
+
+    @Test
+    void 'invalid project id reports error message'() {
+        DeleteScriptAction action = createDeleteAction()
+        when(projectManager.findProjectById('projectId')).thenReturn(null)
+
+        action.process(request, response, ajaxResponse)
+
+        def error = ajaxResponse.getChild('errors').children.get(0) as Element
+        assertThat(error.text, equalTo('File init.gradle cannot be deleted. Project projectId was not found'))
+    }
+
+    @Test
+    void 'user without edit permission reports error message'() {
+        DeleteScriptAction action = createDeleteAction()
+        when(projectManager.findProjectById('projectId')).thenReturn(mock(SProject))
+        when(authorityHolder.isPermissionGrantedForProject(anyString(), eq(Permission.EDIT_PROJECT))).thenReturn(false)
+
+        action.process(request, response, ajaxResponse)
+
+        def error = ajaxResponse.getChild('errors').children.get(0) as Element
+        assertThat(error.text, equalTo('You do not have permissions to edit project settings'))
+    }
+
+    @Test
+    void 'exception during delete reports error message'() {
+        DeleteScriptAction action = createDeleteAction()
+        when(projectManager.findProjectById('projectId')).thenReturn(mock(SProject))
+        when(scriptsManager.deleteScript(any(), any())).thenThrow(AccessDeniedException)
+
+        action.process(request, response, ajaxResponse)
+
+        def error = ajaxResponse.getChild('errors').children.get(0) as Element
+        assertThat(error.text, equalTo('Gradle init script init.gradle cannot be deleted.'))
     }
 }
